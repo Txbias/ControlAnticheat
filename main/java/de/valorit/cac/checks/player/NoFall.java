35578cf67bb9a5a205e1e78a6b2fb0cd2b0b98a0
Txbias <PositivesBanane@gmail.com>
2020-04-23 12:26:52 +0200

    remove debug print statements

--- main/java/de/valorit/cac/checks/player/NoFall.java
+++ main/java/de/valorit/cac/checks/player/NoFall.java
@@ -22,30 +22,24 @@ public class NoFall {
 
     public CheckResult performCheck(PlayerMoveEvent e) {
 
-        System.out.println(1);
-
         Player p = e.getPlayer();
 
         if(p.hasPermission(Permissions.BYPASS)) {
-            System.out.println(2);
            return PASS;
         }
 
         if(!lastFallDistance.containsKey(p)) {
-            System.out.println(3);
             lastFallDistance.put(p, (double) p.getFallDistance());
             lastHealth.put(p, p.getHealth());
         }
 
         if(p.getFallDistance() == 0 && lastFallDistance.get(p) > 3) {
             //Player has fallen
-            System.out.println(4);
 
             Material underPlayer = PlayerUtils.getBlockMaterialUnderPlayer(p);
 
             if(!(PlayerUtils.isInCobweb(p)) && !(PlayerUtils.isInLiquid(p)) && !(underPlayer== Material.SLIME_BLOCK)
                 && !(underPlayer == Material.HAY_BLOCK) && !(underPlayer == Material.SPONGE)) {
-                System.out.println(5);
 
                 User user = CheckResultsManager.getUser(p);
                 long lastDeath = user.getLastDeath();
@@ -54,8 +48,6 @@ public class NoFall {
                 double requiredDamage = (lastFallDistance.get(p) * 0.5) - 1.5;
                 if(requiredDamage > damageTaken && System.currentTimeMillis() - lastDeath > 3000) {
                     //Player is hacking
-
-                    System.out.println(System.currentTimeMillis() - lastDeath);
 
                     double difference = requiredDamage - damageTaken;
                     if(difference > 3) {
